d91ea98d9c76e5b38ab3d0606f0c70a7153b2025
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-12-07 23:52:58 +0300

    Completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    with open(str(file_name)+".txt", mode="w", encoding="utf-8") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(str(file_name)+".txt", mode="a", encoding="utf-8") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(str(file_name)+".txt", encoding="utf-8") as file:
+        return file.read()
+    
+    
+write_file(file_name="test", file_content="Log 1: Samuel\n" )
+append_file(file_name="test", append_content="Log 2: Kovu")
+
+print(read_file(file_name="test"))
